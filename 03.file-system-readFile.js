613cadc861e68585a42c6611af83cbb6e37d49c8
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2025-08-28 22:16:30 -0500

    Modulo nativo FS: readFile para leer archivos de forma Asincrona con readFile utilizando callbacks  que es una funcion que se ejeucta cuando una tarea asincrona a terminado

--- 03.file-system-readFile.js
+++ 03.file-system-readFile.js
@@ -1,13 +1,20 @@
 const fs = require('node:fs') // modulo nativo para trabajar con el sistema de archivos
 
 console.log('Leyendo el primer archivo... ')
-const text = fs.readFileSync('archivo.txt', 'utf-8') // lee el archivo de forma sincrona, si no existe lanza un error
 
-console.log(text);
+fs.readFile('archivo.txt', 'utf-8', (err, text) => { // Lee el archivo,  lo codifica en utf-8, cuando termine sigue ejecutando el codigo del callback
+    console.log('primer texto:', text);
+}) // lee el archivo de forma asincrona, si no existe lanza un error. Lee 
 
+// Mientra lee el archivo esto se ejecuta sin esperar que termine de leer el archivo
 console.log('Hacer cosas mientras lee el archivo...');
 
 console.log("Leyendo el segundo archivo... ");
-const secondText = fs.readFileSync("archivo2.txt", "utf-8");
+fs.readFile("archivo2.txt", "utf-8", (err, text) => {
+    if (err) {
+        console.error("Error al leer el segundo archivo:", err);
+        return;
+    }
+    console.log('segundo texto:', text);
+});
 
-console.log(secondText);
